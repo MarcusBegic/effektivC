7a683dc795732488dd7cc531e7517e214939d3f0
fr3632ho <fredrik.dannert@gmail.com>
2021-11-15 16:17:27 +0100

    poopy pants

--- lab2/simplex.c
+++ lab2/simplex.c
@@ -4,6 +4,7 @@
 #define M 20
 #define N 20
 #define eps 10e-6
+#define INF 10e12
 
 
 double** make_matrix(int m, int n)
@@ -60,7 +61,7 @@ int init(struct simplex_t * s, int m, int n, double ** a, double *b, double *c, 
   return k;
 }
 
-int select_nonbasic(simplex_t * s){
+int select_nonbasic(struct simplex_t *s){
   int i;
   for(i=0; i< s->n; i++){
     if(s->c[i] > eps){
@@ -76,7 +77,110 @@ int initial(struct simplex_t * s, int m, int n, double ** a, double *b, double *
   return 1;
 }
 
-void pivot(simplex_t * s, )
+void pivot(struct simplex_t * s, int row, int col) {
+  double** a = s->a;
+  double* b = s->b;
+  double* c = s->c;
+
+  int m = s->m;
+  int n = s->n;
+  int i,j,temp;
+
+  temp = s->var[col];
+  s->var[col] = s->var[n+row];
+  s->var[n+row] = temp;
+  s->y = s->y + c[col] * b[row] / a[row][col];
+  for (i = 0; i<n; i++) {
+    if(i != col)
+      c[i] = c[i] - c[col] * a[row][i] / a[row][col];
+       
+  }
+
+  c[col] = -c[col] / a[row][col];
+  for (i = 0; i < m; i++) {
+    if (i != row) 
+      b[i] = b[i] - a[i][col] * b[row] / a[row][col];
+  }
+
+  for (i = 0; i < m; i++) {
+    if(i != row) {
+      for(j = 0; j < n; j++) {
+        if (j != col) {
+          a[i][j] = a[i][j] - a[i][col] * a[row][j] / a[row][col];
+        }
+      }
+    }
+  }
+
+  for(i=0; i<n; i++) {
+    if (i != col) {
+      a[row][i] = a[row][i] / a[row][col];
+    }
+  }
+
+  b[row] = b[row] / a[row][col];
+  a[row][col] = 1 / a[row][col];
+
+}
+
+
+double xsimplex(int m, int n, double** a, double* b, double* c, double* x, double y, int* var, int h) {
+  struct simplex_t *s;
+  int i, row, col;
+
+  initial(s, m, n, a, b, c, x, y, var);
+
+  while(1) {
+    col = select_nonbasic(s);
+    if (col < 0) {
+      break;
+    }
+    for (i = 0; i < m; i++) {
+      if (a[i][col] > eps && row < 0 && b[i]/a[i][col] < b[row] / a[row][col]) {
+        row = i;
+      }
+    }
+
+    if (row < 0) {
+      //free(s->var);
+      return INF;
+    }
+
+    pivot(s, row, col);    
+
+  }
+
+  if (h==0) {
+    for(i = 0; i < n; i++) {
+      if (s->var[i] < n) {
+        x[s->var[i]] = 0;
+      }
+    }
+
+    for(i = 0; i < m; i++) {
+      if (s->var[n+i] < n) {
+        x[s->var[n+i]] = s->b[i];
+      }
+    }
+
+    //free(s->var);
+  } else {
+    for(i = 0; i < n; i++)
+      x[i] = 0;
+
+    for(i = n; i<n+m; i++)
+      x[i] = s->b[i-n];  
+  }
+
+  return s->y;
+  
+}
+
+
+double simplex(int m, int n, double** a, double* b, double* c, double* x, double y) {
+  return xsimplex(m, n, a, b, c, x, y, NULL, 0);
+}
+
 
 int main(){
 
@@ -119,7 +223,7 @@ int main(){
 
   struct simplex_t *s  = malloc(sizeof (struct simplex_t));
 
-  init(s, m, n, a, b, c, x, 0, NULL);
+  //init(s, m, n, a, b, c, x, 0, NULL);
 
   /* printf("%d", s->m); */
   /* printf("%d", s->n); */
@@ -127,6 +231,8 @@ int main(){
   /* for(int i=0; i<m+n+1; i++){ */
   /*   printf("%d", s->var[i]); */
   /* } */
+  double y = simplex(m, n, a, b, c, x, 0.0);
+  printf("%lf", y);
 
   return 0;
 }
